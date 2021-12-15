f39ba7f335eb911935e6a555c2a13177bac85c89
rassi0429 <taitokokoa+rassi@gmail.com>
2021-12-15 10:43:01 +0900

    feat: change endpoint

--- main.js
+++ main.js
@@ -9,24 +9,20 @@ const searcher = new fuzzy(names, [], { sort: true })
 const j2e = require("json2emap")
 const express = require("express")
 const app = express();
-app.get("/", (req, res) => {
+app.get("/logix", (req, res) => {
     if (!req.query.q || !req.query.type) {
         res.status(400).send("BAD_REQUEST")
         return
     }
 
-    if (req.query.type == "logix") {
-        let result = searcher.search(req.query.q)
-        if (req.query.details) {
-            result = result.map((r) => { return logix.find(e => e.name === r) })
-            if (req.query.details != "full") {
-                result = result.map(r => { return _.pick(r, ["name", "fullName", "pathName", "types"]) })
-            }
+    let result = searcher.search(req.query.q)
+    if (req.query.details) {
+        result = result.map((r) => { return logix.find(e => e.name === r) })
+        if (req.query.details != "full") {
+            result = result.map(r => { return _.pick(r, ["name", "fullName", "pathName", "types"]) })
         }
-        res.send(req.query.emap ? j2e(result) : result)
-    } else {
-        res.send([])
     }
+    res.send(req.query.emap ? j2e(result) : result)
 })
 
 
